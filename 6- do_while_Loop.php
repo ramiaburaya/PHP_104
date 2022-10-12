eec79e5f5367b3c388dfb0dd9f88853775b35c6d
Aburayya <ramiaburaya16@gmail.com>
2022-10-12 16:43:37 +0300

    [Do While] practice on do_while

---
+++ 6- do_while_Loop.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>PHP do while Loop</title>
+</head>
+
+<body>
+
+    <?php
+
+    echo "<h1>PHP do while Loop</h1>";
+
+    $x = 1;
+
+    do {
+        echo "Value of x is " . $x . "<br><br>";
+        $x++;
+    } while ($x < 5);
+
+
+    ?>
+</body>
+
+</html>
