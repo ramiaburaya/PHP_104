8692c1156ab99bf5ce75d9b22d1cf63f6bfbab5f
Aburayya <ramiaburaya16@gmail.com>
2022-10-12 21:02:26 +0300

    [for loop] {Practice on For loop}

---
+++ 7- For_loop.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>PHP for Loop</title>
+</head>
+
+<body>
+    <?php
+
+    echo "<h1>PHP for Loop</h1>";
+
+    for ($i = 0; $i <= 100; $i++) {
+        echo "Value of <strong>I</string> is <mark>" . $i . "</mark><br><br><hr>";
+    }
+
+    echo "The last value of <strong>I</string> is " . $i;
+
+    ?>
+</body>
+
+</html>
